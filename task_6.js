02675701093c6ca4ac6b7945803ebfe288f52484
Your Name <ahmadsooroosh@icloud.com>
2022-06-10 21:38:18 +0500

    last commit

---
+++ task_6.js
@@ -0,0 +1,7 @@
+//  abramiyan begin 19
+let x1 = prompt(`give x1 of rectangel`)
+let x2 = prompt(`give x2 of rectangel`)
+let y1 = prompt(`give y1 of rectangel`)
+let y2 = prompt(`give y2 of rectangel`)
+let p = alert(`the primeter is ${Math.abs(x1 - x2) + Math.abs(y1 - y2)}`)
+let s = alert(`the  space ${Math.abs(x1 - x2) * Math.abs(y1 - y2)}`)
---
+++ task_7.js
@@ -0,0 +1,6 @@
+//  abramiyan begin 20
+let x1 = prompt(`give x1 of rectangel`)
+let x2 = prompt(`give x2 of rectangel`)
+let y1 = prompt(`give y1 of rectangel`)
+let y2 = prompt(`give y2 of rectangel`)
+let sqr = alert(`the sqrt distance is ${Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) )}`)
