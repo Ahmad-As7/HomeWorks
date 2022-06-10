6c87a2b0952e676256d55e4be59e458bd217be0b
Your Name <ahmadsooroosh@icloud.com>
2022-06-10 20:43:43 +0500

    task_1

---
+++ task_1.js
@@ -0,0 +1,9 @@
+let pi = Math.PI
+let r1 = +prompt("Give R1");
+let s1 = pi * Math.pow(r1,2)
+alert(s1);
+let r2 = +prompt("Give R2");
+let s2 = r2 * Math.pow(r2,2)
+alert(s2);
+let s3 = alert(`the result is ${s2 - s1}`);
+
