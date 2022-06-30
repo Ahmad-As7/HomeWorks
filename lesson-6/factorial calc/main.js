d1165e6fcac7caecb1b89e58243796c2aaca578d
Your Name <ahmadsooroosh@icloud.com>
2022-06-30 14:10:42 +0500

    factorial calculator

---
+++ lesson-6/factorial calc/main.js
@@ -0,0 +1,22 @@
+let num = document.querySelector(".num")
+let btn = document.querySelector(".btn")
+res = document.createElement("div")
+document.body.appendChild(res)
+
+
+btn.onclick = () =>{
+    result = 1
+    for (let i = 1; i <= num.value;i++) {
+        result = result * i
+    }
+     res.innerHTML = result
+}
+
+
+// let factorial = function(n){
+    //     if(n==0){
+    //          return 1
+    //         }
+    //   return  n   * factorial(n-1)
+    // }
+    // res.innerHTML = factorial
