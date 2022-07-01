9132904d2a26bf2afe7ec577cc2c6a7ebcef78ec
Your Name <ahmadsooroosh@icloud.com>
2022-07-01 08:52:35 +0500

    added reset button to counter

--- lesson-6/counter/main.js
+++ lesson-6/counter/main.js
@@ -3,6 +3,7 @@ div1.style.display = "flex";
 div1.style.alignItems = "center";
 div1.style.justifyContent = "center";
 div1.style.marginTop = "20%"
+// div1.style.justifyContent = ""
 let btn1 = document.querySelector(".btn1")
 btn1.classList.add("btn-green")
 let btn2 = document.querySelector(".btn2")
@@ -10,12 +11,17 @@ btn2.classList.add("btn-red")
 let span = document.querySelector("span")
 let brandBtn= document.querySelector(".btn-green")
 brandBtn.style.backgroundColor = "green"
-brandBtn.style.padding = "10px"
+brandBtn.style.padding = "10px 25px"
 brandBtn.style.borderRadius = "7%"
 let brandBtn2= document.querySelector(".btn-red")
 brandBtn2.style.backgroundColor = "red"
-brandBtn2.style.padding = "10px"
+brandBtn2.style.padding = "10px 25px"
 brandBtn2.style.borderRadius = "7%"
+let resetBtn = document.querySelector(".resetBtn")
+resetBtn.style.backgroundColor = "black"
+resetBtn.style.color = "white"
+resetBtn.style.padding = "10px"
+resetBtn.style.borderRadius = "7%"
 
 
 
@@ -40,4 +46,9 @@ btn2.onclick = () =>{
     }
     counter--;
     span.innerHTML = counter
+}
+resetBtn.onclick = () => {
+    span.style.color = "black"
+    counter = 0
+    span.innerHTML = 0
 }
